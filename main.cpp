f24dfb79c90f7562b7ffe11c3bf1b00376317012
ricksterhd123 <ricksterhd123@gmail.com>
2020-05-21 22:06:25 +0100

    added glm to lib folder

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include "sqrt.h"
+#include "glad/glad.h"
+#include <stdlib.h>
+#include <string>
 #include <iostream>
 #include <GLFW/glfw3.h>
+
+// Is called whenever a key is pressed/released via GLFW
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+    std::cout << key << std::endl;
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
+// Window dimensions
+const GLuint WIDTH = 800, HEIGHT = 600;
 
 int main(void)
 {
@@ -9,6 +23,12 @@ int main(void)
     /* Initialize the library */
     if (!glfwInit())
         return -1;
+
+    // Set all the required options for GLFW
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
@@ -21,9 +41,21 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+        // Set the required callback functions
+    glfwSetKeyCallback(window, key_callback);
+
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize OpenGL context" << std::endl;
+        return -1;
+    }
+
+    // Define the viewport dimensions
+    glViewport(0, 0, WIDTH, HEIGHT);
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
+        glClearColor(0.0f, 0.0f, 1.0f, 0.0f);
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
